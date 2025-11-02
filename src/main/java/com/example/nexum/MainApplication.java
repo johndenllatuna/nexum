a36e72d52c63b48619e378ab9d8de275933e9d29
Johnden Llatuna <jllatuna30115@liceo.edu.ph>
2025-11-02 22:02:16 +0800

    nexum

--- src/main/java/com/example/nexum/MainApplication.java
+++ src/main/java/com/example/nexum/MainApplication.java
@@ -2,15 +2,14 @@ package com.example.nexum;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
         stage.setTitle("Hello!");
         stage.show();
     }
