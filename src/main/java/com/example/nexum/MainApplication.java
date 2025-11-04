1e84b30a4f3752079c688dad8fd74f60187c2f93
Johnden Llatuna <jllatuna30115@liceo.edu.ph>
2025-11-04 08:41:17 +0800

    loginFeature

--- src/main/java/com/example/nexum/MainApplication.java
+++ src/main/java/com/example/nexum/MainApplication.java
@@ -2,6 +2,7 @@ package com.example.nexum;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -9,8 +10,12 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        scene.getStylesheets().add(
+                MainApplication.class.getResource("application.css").toExternalForm()
+        );        stage.setTitle("Nexum");
         stage.show();
     }
 
